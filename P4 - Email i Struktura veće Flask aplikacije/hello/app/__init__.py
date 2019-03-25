3a062d9eb84f45adbb83f525aa3af889bf310917
unizd <43952009+unizd@users.noreply.github.com>
2019-03-25 22:58:20 +0100

    Update __init__.py

--- P4 - Email i Struktura veće Flask aplikacije/hello/app/__init__.py
+++ P4 - Email i Struktura veće Flask aplikacije/hello/app/__init__.py
@@ -15,8 +15,4 @@ def create_app(config_name):
 	bootstrap.init_app(app)
 	db.init_app(app)
 	
-	# attach routes and custom error pages here
-	from .main import main as main_blueprint
-	app.register_blueprint(main_blueprint)
-	
 	return app
