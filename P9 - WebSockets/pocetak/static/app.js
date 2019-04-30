f86e6b4ea1f6b7eae70a384fd8bdf51b7884070e
Niko Vrdoljak <niko.vrdoljak@hotmail.com>
2019-04-30 09:27:58 +0200

    P9, WebSockets, SocketIO, Flask-SocketIO, početak

---
+++ P9 - WebSockets/pocetak/static/app.js
@@ -0,0 +1,16 @@
+(function() {
+    var socket = io();
+    // var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
+    // var form = document.querySelector("form");
+    // form.addEventListener("submit", e => {
+    //   e.preventDefault(); // prevents page reloading
+    //   socket.emit('chat_message', document.querySelector('#m').value);
+    //   document.querySelector('#m').value = '';
+    //   return false;
+    // });
+    // socket.on('chat_message', function(msg){
+    //   var li = document.createElement("li");
+    //   li.appendChild(document.createTextNode(msg));
+    //   document.querySelector('#messages').appendChild(li);      
+    // });
+  })();
